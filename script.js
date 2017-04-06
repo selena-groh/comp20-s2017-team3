3ab952e2c0c874d81618ce24c6e5df10b65fce48
bmachlin <ben@machlin.net>
2017-04-06 01:37:11 -0400

    flipgravity + comments

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const EMPTY = 0; PLAYER_1 = 1, PLAYER_2 = 2;
+var gravity = true;
 
 var emptyBoard = [
   [0, 1, 2, 3, 4, 5],
@@ -39,16 +40,56 @@ function printBoard() {
   game.innerHTML = game_text;
 }
 
+// flip gravity and recalculate pieces on the board
 function flipGravity() {
-  var tempBoard = JSON.parse(JSON.stringify(board));
   
   for (c = 0; c < board.length; c += 1) {
-    for (r = 0; r < board[c].length; r += 1) {
-      board[c][board[0].length - 1 - r] = tempBoard[c][r];
+
+    colLen = board[c].length;
+    shift = 0;
+
+    if(gravity) { // gravity normal
+
+      // calculate number of slots to shift pieces
+      for (r = colLen - 1; r >= 0; r -= 1) {
+        if(board[c][r] == 0)
+          shift++;
+        else
+          break;
+      }
+
+      // swap pieces with empty slots
+      if(shift < colLen && shift > 0) {
+        pieceSlice = board[c].slice(0, colLen - shift);
+        emptySlice = board[c].slice(colLen - shift, colLen);
+        board[c] = emptySlice.concat(pieceSlice);
+      }
+
+    } else { // gravity upside down
+
+      // calculate number of slots to shift pieces
+      for (r = 0; r < colLen; r += 1) {
+        if(board[c][r] == 0)
+          shift++;
+        else
+          break;
+      }
+
+      // swap pieces with empty slots
+      if(shift < colLen && shift > 0) {
+        pieceSlice = board[c].slice(shift, colLen);
+        emptySlice = board[c].slice(0, shift);
+        board[c] = pieceSlice.concat(emptySlice);
+      }
+
     }
+
   }
+
+  gravity = !gravity;
 }
 
+// add a piece belonging to player to column c
 function addToCol(player, c) {
   for (r = 0; r < board[c].length; r += 1) {
     if (board[c][r] === EMPTY) {
