0656f0be9d539d728f30b330300396975e49b314
caroline sheng <carsheng@gmail.com>
2017-04-29 19:36:41 -0400

    refiguring for heroku

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
-
 app.set('port', (process.env.PORT || 5000));
 
 // Mongo initialization and connect to database
@@ -19,63 +16,9 @@ var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
 });
 
-
-app.use(express.static(__dirname + '/public'));
-
-// Mount the middleware at "/static" to serve static content only when their request path is prefixed with "/static".
-
-    // GET /static/style.css etc.
-    app.use('/static', express.static(__dirname + '/public'));
-
-
-//app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/src'));
 app.use('/lib', express.static(__dirname + '/lib'));
 app.use('/res', express.static(__dirname + '/res'));
-//app.use('/css',express.static(__dirname + '/float_four.css'));
-app.use('/js',express.static(__dirname + '/js'));
-
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
-});
-
-app.get('/play.html', function(req,res) {
-    res.sendFile(__dirname+'/play.html');
-});
-
-
-io.on('connection',function(socket){
-    socket.on('newplayer',function(){
-        socket.player = {
-           //mongodb id?
-        };
-        socket.emit('allplayers',getAllPlayers());
-        socket.broadcast.emit('newplayer',socket.player);
-    });
-});
-
-function getAllPlayers(){
-    var players = [];
-    Object.keys(io.sockets.connected).forEach(function(socketID){
-        var player = io.sockets.connected[socketID].player;
-        if(player) players.push(player);
-    });
-    return players;
-}
-
-
-io.sockets.on('connection', function(socket) {
-    // once a client has connected, we expect to get a ping from them saying what room they want to join
-    socket.on('room', function(room) {
-        socket.join(room);
-    });
-});
-
-
-room = "abc123";
-io.sockets.in(room).emit('message', 'room joined');
-
-// this message will NOT go to the client defined above
-
 
 /*
 app.post('/newgame', function(request, response) {
