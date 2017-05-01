a7e4930c47c72002e331fde4407b948955ca8f44
bmachlin <ben@machlin.net>
2017-05-01 16:19:13 -0400

    integrating mongo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var db;
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,15 +11,28 @@ app.set('port', (process.env.PORT || 5000));
 // process.env.MONGOHQ_URL is the environment variable on Heroku for the MongoHQ add-on
 // If environment variables not found, fall back to mongodb://localhost/nodemongoexample
 // nodemongoexample is the name of the database
-var mongoUri = process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/nodemongoexample';
+var mongoUri = process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL;
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
-var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
-	db = databaseConnection;
+var db = MongoClient.connect(mongoUri, function(error, dC) {
+	if(err) return;
+
+	db = dc.createCollection('stats', function(err, collection) {});
 });
+
+function insertWin(player, placed) {
+	db.insert({
+		winner: player,
+		time: Date.now(),
+		pieces: placed
+	});
+}
 
 app.use(express.static(__dirname + '/src'));
 app.use('/lib', express.static(__dirname + '/lib'));
 app.use('/res', express.static(__dirname + '/res'));
+// https://github.com/tuftsdev/WebProgramming/blob/gh-pages/examples/nodejs/nodemongoapp/server.js
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
 app.post('/newgame', function(request, response) {
--- src/stats.js
+++ src/stats.js
@@ -1,39 +1,39 @@
 // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['corechart']});
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
 
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-        // Create the data table.
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+  // Create the data table.
 
-        var blue = 10;
-        var red = 25;
+  var blue = 10;
+  var red = 25;
 
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Color');
-        data.addColumn('number', 'Wins');
-        data.addRows([
-          ['Blue', blue],
-          ['Red', red],
-        ]);
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Color');
+  data.addColumn('number', 'Wins');
+  data.addRows([
+    ['Blue', blue],
+    ['Red', red],
+  ]);
 
-        // Set chart options
-        var options = {backgroundColor: 'transparent',
-                        colors: ['#20a3d8', '#ea3347'],
-                        chartArea: { left: '10%', top: '0%', width: "100%", height: "100%" },
-                        legend: {textStyle: {color: 'white', fontSize: 16}},
-                     };
+  // Set chart options
+  var options = {backgroundColor: 'transparent',
+                  colors: ['#20a3d8', '#ea3347'],
+                  chartArea: { left: '10%', top: '0%', width: "100%", height: "100%" },
+                  legend: {textStyle: {color: 'white', fontSize: 16}},
+               };
 
-        //options.legend = {position: 'top', textStyle: {color: 'white', fontSize: 16}},
-        //options.chartArea = { left: '10%', top: '10%', width: "100%", height: "100%" };
+  //options.legend = {position: 'top', textStyle: {color: 'white', fontSize: 16}},
+  //options.chartArea = { left: '10%', top: '10%', width: "100%", height: "100%" };
 
 
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
